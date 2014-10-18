06edf45eea02daa7588c383a6d96622ed5dbb111
DongTianyu <dtianyu@outlook.com>
2014-10-18 22:16:10 +0800

    加入PDF显示功能

--- WJS/WJS-ejb/build/generated-sources/ap-source-output/com/wjs/entity/PartyGroup_.java
+++ WJS/WJS-ejb/build/generated-sources/ap-source-output/com/wjs/entity/PartyGroup_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-16T21:52:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-18T00:46:17")
 @StaticMetamodel(PartyGroup.class)
 public class PartyGroup_ { 
 
